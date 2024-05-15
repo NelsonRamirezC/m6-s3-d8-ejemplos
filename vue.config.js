4c1ec8d1a5ed499604c8428d6a82de8790d63db6
Nelson Ramirez <xhyskoax@gmail.com>
2024-05-15 12:18:18 -0400

    iniciando proyecto

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: "/m6-s3-d8-ejemplos/"
 })
